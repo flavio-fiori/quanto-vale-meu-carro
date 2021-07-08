c83c0f88d7d2ecbcb3ad382ca85ea2a0eab9970b
Flávio <flavio.fiori31@gmail.com>
2021-07-08 00:44:24 -0300

    :rocket: feat(project/metastag): add styling seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,8 @@ export default class MyDocument extends Document {
 
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap" rel="stylesheet"/>
-
+                    <meta name="theme-color" content="#228855"/>
+                    <title>Quanto vale meu carro?</title>
 
                 </Head>
 
